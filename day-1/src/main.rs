3a87fd95ec866cbab0c270e49c82de68da661e26
MouadBNL <mouadbnl.2k01@gmail.com>
2024-12-01 12:07:07 +0100

    solved day 1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -21,20 +21,23 @@ fn main() {
             x.1.trim().parse::<i32>().unwrap()
         })
         .collect();
-    // a.sort();
-    // b.sort();
+    a.sort();
+    b.sort();
 
     let freq = b.iter().copied().fold(HashMap::new(), |mut map, val| {
         map.entry(val).and_modify(|f| *f += 1).or_insert(1);
         map
     });
-    let mut ans = 0;
+    let mut ans1 = 0;
+    let mut ans2 = 0;
     for i in 0..a.len() {
-        ans = ans + a[i] * freq.get(&a[i]).unwrap_or(&0);
+        ans1 += (a[i] - b[i]).abs();
+        ans2 += a[i] * freq.get(&a[i]).unwrap_or(&0);
         // println!(
         //     "Ans itr {}, a[i] = {}, b[i] = {}, ans = {}",
         //     i, a[i], b[i], ans
         // );
     }
-    println!("Ans: {}.", ans);
+    println!("Part 1: {}.", ans1);
+    println!("Part 2: {}.", ans2);
 }
