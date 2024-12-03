3654bae8b0658b83b243aa3fbd4ccaeddf4265f7
MouadBNL <mouadbnl.2k01@gmail.com>
2024-12-03 11:16:47 +0100

    chore: cleanup

--- day-2/target/debug/day-2.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mapping\Projects\AdventOfCode2024\day-2\target\debug\day-2.exe: C:\Users\mapping\Projects\AdventOfCode2024\day-2\src\main.rs
--- day-2/target/debug/deps/day_2-c08b1cef20f4a0bf.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mapping\Projects\AdventOfCode2024\day-2\target\debug\deps\day_2-c08b1cef20f4a0bf.exe: src/main.rs
-
-C:\Users\mapping\Projects\AdventOfCode2024\day-2\target\debug\deps\day_2-c08b1cef20f4a0bf.d: src/main.rs
-
-src/main.rs:
